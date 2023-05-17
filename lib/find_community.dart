4844bd726502651a2720ea7f120b9d66bda16ba9
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-05-17 04:26:12 +0300

    latitude and longitude values to address

--- lib/find_community.dart
+++ lib/find_community.dart
@@ -1,53 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
 class FindCommunity extends StatefulWidget {
+  const FindCommunity({Key? key}) : super(key: key);
+
   @override
-  _FindCommunityState createState() => _FindCommunityState();
+  State<FindCommunity> createState() => _FindCommunityState();
 }
 
 class _FindCommunityState extends State<FindCommunity> {
+  String? _currentAddress;
   Position? _currentPosition;
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+      _getAddressFromLatLng(_currentPosition!);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
+  Future<void> _getAddressFromLatLng(Position position) async {
+    await placemarkFromCoordinates(
+        _currentPosition!.latitude, _currentPosition!.longitude)
+        .then((List<Placemark> placemarks) {
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+        '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
+      });
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
-        title: const Text('Topluluk Bul',style: TextStyle(fontWeight:FontWeight.bold),),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_currentPosition != null)
-              Text(
-                  'Latitude: ${_currentPosition!.latitude}, Longitude: ${_currentPosition!.longitude}'),
-            ElevatedButton(
-              onPressed: () async {
-                LocationPermission permission =
-                await Geolocator.requestPermission();
-
-                if (permission == LocationPermission.denied ||
-                    permission == LocationPermission.deniedForever) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text(
-                          'Konum izni verilmedi. Ayarlardan izin vermeniz gerekiyor.'),
-                    ),
-                  );
-                  return;
-                }
-
-                Position position =
-                await Geolocator.getCurrentPosition();
-                setState(() {
-                  _currentPosition = position;
-                });
-              },
-              child: const Text('Konum İzni Al'),
-            ),
-          ],
+          title: const Text("Topluluk Bul",style: TextStyle(fontWeight: FontWeight.bold),)),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Adres : ${_currentAddress ?? ""}'),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: _getCurrentPosition,
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
+                child: const Text("Konumu Etkinleştir"),
+              )
+            ],
+          ),
         ),
       ),
     );
