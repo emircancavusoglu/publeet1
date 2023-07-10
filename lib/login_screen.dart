dcc6e562ab920abd43baed5df7f8f14172b3a843
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-07-10 22:04:24 +0300

    update login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:publeet1/forget_password.dart';
-import 'package:publeet1/services/auth_services.dart';
-import 'package:publeet1/sign_register_form.dart';
-import 'selection_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,173 +8,116 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController userNameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  Future<void> navigateToRegistrationScreen() async {
-    await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SelectionScreen()),
-    ).then((value) {
-      userNameController.clear();
-      passwordController.clear();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
               "Publeet",
               style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
                 color: Colors.indigoAccent,
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 24),
-            TextField(
-              controller: userNameController,
-              decoration: InputDecoration(
-                hintText: 'E-Posta',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-            const SizedBox(height: 12),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                hintText: 'Şifre',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 100),
             ElevatedButton(
               onPressed: () {
-                AuthServices().signIn(
-                  context,
-                  email: userNameController.text,
-                  password: passwordController.text,
-                );
-                userNameController.clear();
-                passwordController.clear();
+                // Handle Google login here
               },
               style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 48),
-                backgroundColor: Colors.deepPurple,
+                minimumSize: const Size(300, 60),
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                backgroundColor: Colors.white,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8.0),
                 ),
               ),
-              child: const Text('Giriş Yap'),
-            ),
-            const SizedBox(height: 12),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ForgetPassword()),
-                );
-              },
-              child: const Text(
-                "Şifreni mi unuttun?",
-                style: TextStyle(
-                  color: Colors.black,
-                ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/google.png',
+                    height: 24,
+                    width: 24,
+                  ),
+                  const SizedBox(width: 12),
+                  const Text(
+                    'Google ile Giriş Yap',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
               ),
             ),
             const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Hesabın yok mu?",
-                  style: TextStyle(
+            ElevatedButton(
+              onPressed: () {
+                // Handle Apple login here
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 60),
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.apple,
                     color: Colors.grey,
+                    size: 30,
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const UserRegistrationScreen()),
-                    );
-                  },
-                  child: const Text(
-                    "Kayıt ol",
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      // Handle Google login here
-                    },
-                    icon: Image.asset(
-                      'assets/google.png',
-                      height: 12,
-                      width: 12,
-                    ),
-                    label: const Text(
-                      'Google ile giriş yap',
-                      style: TextStyle(
-                        color: Colors.black87,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(48, 48),
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 12),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Handle Apple login here
-                  },
-                  icon: const Icon(Icons.apple,color: Colors.grey,),
-                  label: const Text(
-                    'Apple ile giriş yap',
+                  SizedBox(width: 12),
+                  Text(
+                    'Apple ile Giriş Yap',
                     style: TextStyle(
                       color: Colors.black87,
+                      fontSize: 18,
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(48, 48),
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                // Handle Facebook login here
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 60),
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-              ],
+              ),
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.facebook,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                  SizedBox(width: 12),
+                  Text(
+                    'Facebook ile Giriş Yap',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
