53337bcfbb2ec5186ccb69762853c7154adb4fea
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-07-17 17:21:06 +0300

    mark location

--- lib/communityWorld.dart
+++ lib/communityWorld.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'location/locations.dart' as locations;
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CommunityScreen extends StatefulWidget {
-   const CommunityScreen({super.key});
+  const CommunityScreen({super.key});
 
   @override
   State<CommunityScreen> createState() => _CommunityScreenState();
@@ -12,6 +12,7 @@ class CommunityScreen extends StatefulWidget {
 
 class _CommunityScreenState extends State<CommunityScreen> {
   final Map<String, Marker> _markers = {};
+
   Future<void> _onMapCreated(GoogleMapController controller) async {
     final googleOffices = await locations.getGoogleOffices();
     setState(() {
@@ -30,6 +31,40 @@ class _CommunityScreenState extends State<CommunityScreen> {
     });
   }
 
+  Future<void> _addCommunityMarkers() async {
+    // Firestore'dan community_requests koleksiyonundan requestStatus değeri true olan toplulukları almak
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('community_requests')
+        .where('requestStatus', isEqualTo: true)
+        .get();
+
+    // snapshot'taki toplulukların konum bilgilerini kullanarak haritada işaretleme yapmak
+    setState(() {
+      _markers.clear();
+      for (var doc in snapshot.docs) {
+        var communityName = doc['communityName'];
+        var communityLat = doc['latitude']; // Topluluğun enlem değeri
+        var communityLng = doc['longitude']; // Topluluğun boylam değeri
+
+        final marker = Marker(
+          markerId: MarkerId(communityName),
+          position: LatLng(communityLat, communityLng),
+          infoWindow: InfoWindow(
+            title: communityName,
+            snippet: doc['communityAddress'], // Topluluğun adres bilgisi
+          ),
+        );
+        _markers[communityName] = marker;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _addCommunityMarkers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -43,10 +78,9 @@ class _CommunityScreenState extends State<CommunityScreen> {
         appBar: AppBar(
           leading: IconButton(
             icon: const Icon(Icons.arrow_back),
-            onPressed: (){
+            onPressed: () {
               Navigator.pop(context);
             },
-
           ),
           title: const Text('Dünyadaki Topluluklar'),
           elevation: 2,
