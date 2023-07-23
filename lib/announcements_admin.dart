3b660407ed4244d62886667077d70056635fdf59
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-07-23 15:16:36 +0300

    add announcement

--- lib/announcements_admin.dart
+++ lib/announcements_admin.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
-class AnnounceAdmin extends StatelessWidget {
+
+class AnnounceAdmin extends StatefulWidget {
   const AnnounceAdmin({Key? key}) : super(key: key);
+
+  @override
+  State<AnnounceAdmin> createState() => _AnnounceAdminState();
+}
+
+class _AnnounceAdminState extends State<AnnounceAdmin> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Duyurularım"),
+        title: const Text("Duyuru Ekle"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(
+                hintText: "Duyuru ekleyin...",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String enteredText = _textEditingController.text;
+                print("Kullanıcının girdiği metin: $enteredText");
+              },
+              child: const Text("Gönder"),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
