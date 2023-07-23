f0566800b63fdbac6a881612b44b8a8fee9a182d
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-07-23 22:18:07 +0300

    add create community page

--- lib/announcements_admin.dart
+++ lib/announcements_admin.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:publeet1/community_details.dart';
 
 class AnnounceAdmin extends StatefulWidget {
   const AnnounceAdmin({Key? key}) : super(key: key);
@@ -8,41 +11,140 @@ class AnnounceAdmin extends StatefulWidget {
 }
 
 class _AnnounceAdminState extends State<AnnounceAdmin> {
-  TextEditingController _textEditingController = TextEditingController();
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  GetData data = GetData();
 
   @override
   Widget build(BuildContext context) {
+    User? currentUser = _auth.currentUser;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Duyuru Ekle"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        backgroundColor: Colors.deepPurple,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Row(
           children: [
-            TextField(
-              controller: _textEditingController,
-              decoration: const InputDecoration(
-                hintText: "Duyuru ekleyin...",
-              ),
+            SizedBox(
+              width: 26,
             ),
-            ElevatedButton(
-              onPressed: () {
-                String enteredText = _textEditingController.text;
-                print("Kullanıcının girdiği metin: $enteredText");
-              },
-              child: const Text("Gönder"),
+            Text("Duyuru Oluştur"),
+            SizedBox(
+              width: 2,
             ),
+            Icon(Icons.group),
           ],
         ),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            color: Colors.white70, // Set the background color for the heading
+            child: const Text(
+              "Topluluk Seç", // Your heading text
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue, // Set the text color for the heading
+              ),
+            ),
+          ),
+          Expanded(
+            // Use the Expanded widget to allow the ListView to take remaining space
+            child: StreamBuilder<List<String>>(
+              stream: data.getData(currentUser!.email.toString()),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<String>? communityNames = snapshot.data;
+                  if (communityNames != null && communityNames.isNotEmpty) {
+                    return ListView.builder(
+                      itemCount: communityNames.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 13),
+                          child: ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => CommunityDetails(communityName: communityNames[index]),
+                                        ),
+                                      );
+                                    },
+                                    child: Text(
+                                      communityNames[index],
+                                      style: const TextStyle(
+                                        fontSize: 23,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 2),
+                                StreamBuilder<QuerySnapshot>(
+                                  stream: FirebaseFirestore.instance
+                                      .collection('community_requests')
+                                      .where('communityName', isEqualTo: communityNames[index])
+                                      .snapshots(),
+                                  builder: (context, snapshot) {
+                                    return const SizedBox();
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: Text(
+                        'Topluluk bulunamadı.',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    );
+                  }
+                } else if (snapshot.hasError) {
+                  return Text('Hata: ${snapshot.error}');
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class GetData {
+  Stream<List<String>> getData(String email) {
+    return FirebaseFirestore.instance
+        .collection('community_requests')
+        .where('userEmail', isEqualTo: email)
+        .where('requestStatus', isEqualTo: true)
+        .snapshots()
+        .map((snapshot) {
+      List<String> communityNames = [];
+      for (var doc in snapshot.docs) {
+        var communityName = doc['communityName'];
+        communityNames.add(communityName);
+      }
+      return communityNames;
+    });
   }
 }
